729f771aaa98617c13d0d173762ce940fa4ee304
Diego <dgvlpt@gmail.com>
2014-04-21 12:59:32 -0300

    first commit - test

---
+++ src/dgvolpato/DiaDePagamento/MyActivity.java
@@ -0,0 +1,17 @@
+package dgvolpato.DiaDePagamento;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        System.out.println("hello");
+    }
+}
